e291a5cee02037539b7fae334c3a19516a4ff7e5
Ian Melendez <ian.melendez.95@gmail.com>
2025-12-03 20:40:19 -0600

    d3p1 short working

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,15 +1,29 @@
-module Day3 where
+module Day3 
+  ( soln, 
+    numbers0,
+    findMaxNum0
+  ) 
+  where
 
 import Data.Text qualified as T
 import Data.Text.IO qualified as TIO
 import Data.Text.Read
 import Data.List (isPrefixOf)
 import Data.List.Split (chunksOf)
+import Data.Maybe (fromMaybe)
+import Text.Read (readMaybe)
 
 soln :: FilePath -> IO Int
 soln file = do
-  content <- TIO.readFile file
-  TIO.putStr content
-  pure 0
+  banks <- T.lines <$> TIO.readFile file
+  pure $ sum (map (findMaxNum0 . T.unpack) banks)
 
+findMaxNum0 :: [Char] -> Int
+findMaxNum0 digits = maximum $ numbers0 digits
 
+numbers0 :: [Char] -> [Int]
+numbers0 [] = []
+numbers0 (d:ds) = map (digitsToNum0 d) ds ++ numbers0 ds
+
+digitsToNum0 :: Char -> Char -> Int
+digitsToNum0 d1 d2 = fromMaybe (error $ show [d1, d2]) $ readMaybe [d1, d2]
--- test/Test/Day3.hs
+++ test/Test/Day3.hs
@@ -23,10 +23,28 @@ testDay3 =
     describe "soln" $ do 
       it "short-input.txt" $ do 
         res <- soln "test/Test/Day3/short.txt"
-        res `shouldBe` 0
+        res `shouldBe` 357
 
       xit "full-input.txt" $ do 
         res <- soln "test/Test/Day3/full.txt"
         -- putStrLn $ "Solution: " ++ show res
         res `shouldBe` 50793864718
+
+      
+      -- 987654321111111
+      -- 811111111111119
+      -- 234234234234278
+      -- 818181911112111
+      it "987654321111111" $ do 
+        findMaxNum0 "987654321111111" `shouldBe` 98
+
+      it "811111111111119" $ do 
+        findMaxNum0 "811111111111119" `shouldBe` 89
+
+      it "234234234234278" $ do 
+        findMaxNum0 "234234234234278" `shouldBe` 78
+
+      it "818181911112111" $ do 
+        findMaxNum0 "818181911112111" `shouldBe` 92
+
 
