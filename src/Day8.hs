61aeca5b476a1f1d4f2729d35945422c3390a515
Ian Melendez <ian.melendez.95@gmail.com>
2025-12-08 08:31:08 -0600

    parse coords

--- src/Day8.hs
+++ src/Day8.hs
@@ -31,11 +31,21 @@ import Data.Text.Read
 import Debug.Trace (trace, traceShowId, traceWith)
 import Text.Read (readMaybe)
 
+data Coord = Coord Int Int Int deriving (Show, Eq)
+
 soln :: FilePath -> IO Int
 soln file = do
   ls <- T.lines <$> TIO.readFile file
+  let coords = map readCoord ls
+  mapM_ print coords
   print ls
   pure 0
+
+readCoord :: T.Text -> Coord
+readCoord line = 
+  case map readInt . T.split (== ',') $ line of 
+    [x, y, z] -> Coord x y z
+    _ -> error $ "parse: " ++ show line
 
 readInt :: T.Text -> Int
 readInt = either error fst . decimal
