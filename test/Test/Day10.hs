ba667a481daeba4228bcafd8d3ff7425f31da061
Ian Melendez <ian.melendez.95@gmail.com>
2025-12-10 20:19:29 -0600

    presses

--- test/Test/Day10.hs
+++ test/Test/Day10.hs
@@ -1,13 +1,14 @@
 module Test.Day10 (test) where
 
+import Data.List
 import Data.Map qualified as M
 import Data.Maybe (fromJust)
+import Data.Ord
 import Data.Set qualified as S
 import Data.Text qualified as T
 import Data.Text qualified as TIO
-import Data.List
-import Data.Ord
 import Day10
+import Parse qualified as P
 import Test.Hspec
   ( Expectation,
     SpecWith (..),
@@ -23,7 +24,6 @@ import Test.Hspec
     xit,
   )
 import Test.QuickCheck
-import Parse qualified as P
 
 shortInput :: FilePath
 shortInput = "test/Test/Day10/short.txt"
@@ -40,20 +40,33 @@ test =
         res <- soln "test/Test/Day10/full.txt"
         putStrLn $ "Solution: " ++ show res
         res `shouldNotBe` 0
-    
-    describe "pressButton" $ do 
-      it "simple press" $ do 
+
+    describe "pressButton" $ do
+      it "simple press" $ do
         pressButton0 (S.fromList [1, 2]) (S.singleton 3) `shouldBe` S.fromList [1, 2, 3]
 
-    describe "pMachine" $ do 
-      it "parses first" $ do 
+      it "first three" $ do
+        -- [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1)
+        let result =
+              foldl'
+                pressButton0
+                S.empty
+                [ S.singleton 3,
+                  S.fromList [1, 3],
+                  S.singleton 2
+                ]
+        result `shouldBe` S.fromList [1, 2]
+
+    describe "pMachine" $ do
+      it "parses first" $ do
         let res = P.parse pMachine "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"
-        res `shouldBe` Mach (S.fromList [1, 2]) [
-            S.singleton 3, 
-            S.fromList [1, 3],
-            S.singleton 2,
-            S.fromList [2, 3],
-            S.fromList [0, 2],
-            S.fromList [0, 1]
-          ]
-        
+        res
+          `shouldBe` Mach
+            (S.fromList [1, 2])
+            [ S.singleton 3,
+              S.fromList [1, 3],
+              S.singleton 2,
+              S.fromList [2, 3],
+              S.fromList [0, 2],
+              S.fromList [0, 1]
+            ]
