ba8a840fcc80b22c0d25bde700f5294ace9f137e
Ian Melendez <ian.melendez.95@gmail.com>
2025-12-11 19:19:56 -0600

    wip d10

--- src/Day10.hs
+++ src/Day10.hs
@@ -4,11 +4,12 @@ module Day10
   ( soln,
     Mach (..),
     pMachine,
-    pressButton0
+    pressButton0,
   )
 where
 
 import Control.Applicative (liftA2)
+import Control.Monad.State
 import Data.List
   ( find,
     foldl',
@@ -32,22 +33,39 @@ import Data.Maybe
     maybe,
   )
 import Data.Ord (Down (..), comparing)
+import Data.Sequence (Seq)
+import Data.Sequence qualified as Seq
 import Data.Set qualified as S
-import Data.Text qualified as T
 import Data.Text (pattern (:<), pattern (:>))
+import Data.Text qualified as T
 import Data.Text.IO qualified as TIO
 import Data.Text.Read
-import Debug.Trace (trace, traceShowId, traceWith)
-import Text.Read (readMaybe)
 import Data.Void
+import Debug.Trace (trace, traceShowId, traceWith)
 import Parse qualified as P
-import Data.Sequence qualified as Seq
-import Data.Sequence (Seq)
+import Text.Read (readMaybe)
 
 type Lights = S.Set Int
+
 type Button = S.Set Int
 
 data Mach = Mach Lights [Button] deriving (Show, Eq)
+
+data MS = MS
+  { univs :: Seq.Seq (Lights, Int),
+    buttons :: [Button],
+    expected :: Lights
+  }
+
+type M = State MS
+
+mkMS :: Mach -> MS
+mkMS (Mach lights buttons) =
+  MS
+    { univs = Seq.singleton (S.empty, 0),
+      buttons,
+      expected = lights
+    }
 
 soln :: FilePath -> IO Int
 soln file = do
@@ -55,6 +73,25 @@ soln file = do
   let machs = map readMachine t_lines
   mapM_ print machs
   pure 0
+
+-- stepM :: M
+-- stepM = do 
+--   cur_lights <- gets curLights
+--   btns <- gets buttons
+--   expected <- gets expected
+--   let next_lights = map (pressButton0 cur_lights) btns
+--   modify ()
+--   if any (== expected) next_lights
+--     then modify ()
+--     else undefined
+
+buttonPresses0 :: Lights -> Lights -> [Button] -> Int
+buttonPresses0 expected current buttons
+  | expected == current = 0
+  | otherwise = undefined
+
+-- let all_presses = map (\b -> buttonPresses0 expected (pressButton0 b) buttons) buttons
+--  in undefined
 
 pressButton0 :: Lights -> Button -> Lights
 pressButton0 = S.symmetricDifference
@@ -65,42 +102,16 @@ readMachine = P.parse pMachine
 pMachine :: P.Parser Mach
 pMachine = Mach <$> P.lexeme pLights <*> P.some (P.lexeme pButton)
 
-pButton :: P.Parser Button 
+pButton :: P.Parser Button
 pButton = S.fromList <$> P.between (P.char '(') (P.char ')') (P.sepBy1 P.decimal (P.symbol ","))
 
 pLights :: P.Parser Lights
 pLights = do
   light_states <- P.between (P.char '[') (P.char ']') $ P.some pLight
-  pure . S.fromList $ map fst . filter snd . zip [0..] $ light_states
+  pure . S.fromList $ map fst . filter snd . zip [0 ..] $ light_states
 
 pLight :: P.Parser Bool
 pLight = (== '#') <$> P.satisfy (\c -> c == '.' || c == '#')
 
 readInt :: T.Text -> Int
 readInt = either error fst . decimal
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
